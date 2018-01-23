6b73efb9f6ecf517a4aa3e27c1624283b8c32582
falcon646 <ashwinutility@gmail.com>
2018-01-23 21:47:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,56 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Ashwin on 09-01-2018.
+ */
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private  int mAudioResourceId;
+
+    public Word(String defaultTranslation, String miwokTranslation, int AudioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = AudioResourceId;
+    }
+
+    public Word(String DefaultTranslation, String MiwokTranslation, int ImageResourceId, int AudioResourceId) {
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = MiwokTranslation;
+        mImageResourceId = ImageResourceId;
+        mAudioResourceId = AudioResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
+    }
+}
